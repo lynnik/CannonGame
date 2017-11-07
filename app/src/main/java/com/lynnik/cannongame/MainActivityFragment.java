37f0425494fa42d0abda7b29614a2324ef7bb1f2
Lynnik Vlad <lynnik.v@gmail.com>
2017-11-07 12:45:23 +0200

    Changed MainActivityFragment class.

--- app/src/main/java/com/lynnik/cannongame/MainActivityFragment.java
+++ app/src/main/java/com/lynnik/cannongame/MainActivityFragment.java
@@ -1,18 +1,44 @@
 package com.lynnik.cannongame;
 
+import android.media.AudioManager;
 import android.os.Bundle;
 import android.app.Fragment;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 public class MainActivityFragment extends Fragment {
 
+  private CannonView mCannonView;
+
   @Override
   public View onCreateView(
       LayoutInflater inflater,
       ViewGroup container,
       Bundle savedInstanceState) {
-    return inflater.inflate(R.layout.fragment_main, container, false);
+    View v = inflater.inflate(R.layout.fragment_main, container, false);
+
+    mCannonView = v.findViewById(R.id.cannonView);
+
+    return v;
+  }
+
+  @Override
+  public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+    super.onActivityCreated(savedInstanceState);
+    getActivity().setVolumeControlStream(AudioManager.STREAM_MUSIC);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    mCannonView.stopGame();
+  }
+
+  @Override
+  public void onDestroy() {
+    super.onDestroy();
+    mCannonView.releaseResources();
   }
 }
