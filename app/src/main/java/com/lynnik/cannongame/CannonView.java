5001195a15e81bd79bbd9c81829a5a11097ba69a
Lynnik Vlad <lynnik.v@gmail.com>
2017-11-07 12:38:35 +0200

    Created CannonView class.

---
+++ app/src/main/java/com/lynnik/cannongame/CannonView.java
@@ -0,0 +1,12 @@
+package com.lynnik.cannongame;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class CannonView extends SurfaceView {
+
+  public CannonView(Context context, AttributeSet attrs) {
+    super(context, attrs);
+  }
+}
